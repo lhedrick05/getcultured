b74eda6925b12336fc11039b4fd29abb23c4f90b
Cresence <devharris93@gmail.com>
2023-12-11 20:51:34 -0500

    Starter files for controller, templates, and static.

---
+++ src/main/java/liftoff/atlas/getcultured/controllers/HomePageController.java
@@ -0,0 +1,17 @@
+package liftoff.atlas.getcultured.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping(value="")
+    @ResponseBody
+    public String renderHomePage () {
+
+        return "index";
+    }
+
+}
