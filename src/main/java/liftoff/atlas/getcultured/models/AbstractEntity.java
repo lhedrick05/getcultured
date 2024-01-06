ab829a1ebd153b6579d18d81c0c62a5baa7df4cf
Landon <landon.hedrick05@gmail.com>
2024-01-06 15:07:36 -0600

    Have basic functionality for tour creation, (need to add more fields to templates and find out the data coming from a user and the data coming from the API). App can update tours as well and I need to add the redirect link to take you back to the tour index page. Next to work on stop creation and deletion.

--- src/main/java/liftoff/atlas/getcultured/models/AbstractEntity.java
+++ src/main/java/liftoff/atlas/getcultured/models/AbstractEntity.java
@@ -1,7 +1,6 @@
 package liftoff.atlas.getcultured.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
